e72ee5d33b373241ccdcfafa833d49852e4bc5f4
Kevin Garnett <kgarnett24@gmail.com>
2022-01-01 23:53:51 -0400

    CLI Player search

---
+++ index.js
@@ -0,0 +1,33 @@
+const prompts = require('prompts');
+const player_lookup = require('./lib/player_lookup');
+
+(async () => {
+  const response = await prompts({
+    type: 'text',
+    name: 'value',
+    message: 'Last Name?',
+    validate: value => value.length < 1 ? `Field can not be blank` : true
+  });
+
+  players = await player_lookup.findByName(response['value'])
+
+  // Remove players with incomplete names
+  players = players.filter(player => player["first_name"] != null);
+
+  const selectPlayer = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Which Player?',
+    choices: players.map((player) => {
+
+      if (player['first_name'] != null) {
+        return {
+          title: `${player['first_name']} ${player['last_name']} ${(player['name_suffix'] == null) ? '' : player['name_suffix']} (born: ${(player['birth_year'] == null) ? '?' : player['birth_year']}, MLB Debut: ${(player['mlb_played_first'] == null) ? '?' : player['mlb_played_first']})`,
+          value: player
+        }  
+      }
+      
+    }),
+    initial: 0
+  });
+})();
